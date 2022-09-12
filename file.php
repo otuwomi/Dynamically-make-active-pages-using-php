7fc1048ec9849f13342c6d903d986943a4eb89ec
Womi Otu <107402278+otuwomi@users.noreply.github.com>
2022-09-12 20:46:43 +0100

    Create file.php

---
+++ file.php
@@ -0,0 +1,64 @@
+<ul class="navbar-nav ml-lg-auto">
+							<li <?php if($_SERVER['PHP_SELF']=='/bankofca/index.php'){ echo('class="nav-item active"');?> <?php }else{echo('class="nav-item"');} ?>>
+								<a class="nav-link" href="index.php" >Home
+									
+								</a>
+							
+							</li>
+							<li <?php if($_SERVER['PHP_SELF']=='/bankofca/security.php'){ echo('class="nav-item mx-xl-4 mx-lg-3 mt-lg-0 mt-3 active"');?> <?php }else{echo('class="nav-item nav-item mx-xl-4 mx-lg-3 mt-lg-0 mt-3"');} ?>>
+								<a class="nav-link" href="security.php">Security</a>
+							</li>
+							<li <?php if($_SERVER['PHP_SELF']=='/bankofca/careers.php'){ echo('class="nav-item nav-item mx-xl-4 mx-lg-3 mt-lg-0 mt-3 active"');?> <?php }else{echo('class="nav-item nav-item mx-xl-4 mx-lg-3 mt-lg-0 mt-3"');} ?>>
+								<a class="nav-link" href="careers.php">Careers</a>
+							</li>
+						
+							<li <?php if($_SERVER['PHP_SELF']=='/bankofca/investment.php'){ echo('class="nav-item nav-item mx-xl-4 mx-lg-3 mt-lg-0 mt-3 active"');?> <?php }else{echo('class="nav-item nav-item mx-xl-4 mx-lg-3 mt-lg-0 mt-3"');} ?>>
+								<a class="nav-link" href="investment.php">Investment</a>
+							</li>
+								<li <?php if($_SERVER['PHP_SELF']=='/bankofca/contact.php'){ echo('class="nav-item nav-item mx-xl-4 mx-lg-3 mt-lg-0 mt-3 active"');?> <?php }else{echo('class="nav-item nav-item mx-xl-4 mx-lg-3 mt-lg-0 mt-3"');} ?>>
+								<a class="nav-link" href="contact.php">Contact Us</a>
+							</li>
+							
+						
+								<li class="nav-item mx-xl-4 mx-lg-3 mt-lg-0 mt-3">
+								<a class="nav-link" href="users/register.php" target="_blank">Signup</a>
+							</li>
+								<li class="search my-lg-0 my-4">
+								<a class="play-icon popup-with-zoom-anim" href="#small-dialog">
+									<i class="fa fa-flag" aria-hidden="true" alt="Change Language"></i>
+								</a>
+								<div id="small-dialog" class="mfp-hide">
+									<div class="search-top">
+										<div class="search-inn">
+										
+										<style>
+<!--
+#google_translate_element img{
+	display:none;
+}
+select#google_translate_element {
+  color: black;
+}
+-->
+</style>
+	<div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="translate.google.com/translate_a/elementa0d8a0d8.html?cb=googleTranslateElementInit"></script>
+										<!--
+											<form action="#" method="post" class="disply-flex">
+												<input class="form-control" type="search" name="search" placeholder="Type something..." required>
+												<button class="btn2">
+													<i class="fa fa-search" aria-hidden="true"></i>
+												</button>
+											</form>-->
+											<p class="text-white font-weight-bold mt-4">Change Language </p>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li class="ml-lg-4 mb-lg-0 mb-4">
+								<a href="users/index.php" class="reqe-button" target="_blank">Login</a>
+							</li>
+						</ul>
